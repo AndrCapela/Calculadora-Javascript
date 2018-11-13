c7f625c819f3b696736356fa57f9bba43602ace3
André Capela <andr.capela@gmail.com>
2018-11-13 11:12:04 -0200

    Iniciando operacoes na calculadora

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -7,6 +7,8 @@ class CalcController {
         this._dateEl = document.querySelector("#data");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
+        this._operation = [];
     }
 
     initialize() {
@@ -21,8 +23,88 @@ class CalcController {
         }, 10000)*/
     }
 
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    setError() {
+        this.displayCalc = "Error";
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+
+                break;
+            case 'subtracao':
+
+                break;
+            case 'divisao':
+
+                break;
+            case 'multiplicacao':
+
+                break;
+            case 'porcento':
+
+                break;
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+        }
+    }
+
     initButtonsEvents() {
         let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+        buttons.forEach(btn => {
+            this.addEventListenerAll(btn, "click drag", e => {
+                let textBtn = btn.className.baseVal.replace('btn-', "");
+
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+        });
     }
 
     setDisplayDateTime() {
